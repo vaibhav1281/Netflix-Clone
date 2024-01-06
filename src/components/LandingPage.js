cb3c7b6f7f6e9132371dce51a14ba2e1e8f7291e
vaibhav1281 <vaibhav1281saroj@gmail.com>
2024-01-06 13:20:20 +0530

    Added Heroicons for the language icon and chevron icon.

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,38 +2,53 @@ import React from 'react'
 import Header from './Header'
 import { Link } from 'react-router-dom'
 import { IN_BACKGROUND_CDN } from '../utils/constant'
+import { ChevronRightIcon } from "@heroicons/react/24/solid"
+import video1 from '../assets/video-tv.m4v' 
+import { TV } from '../assets/assets'
+
 
 export const LandingPage = () => {
   return (
-    <div className='w-full'>
-    <div className="absolute inset-0 bg-gradient-to-top h-[750px]"></div>
+    <div className='relative w-full'>
+        <div className="absolute inset-0 bg-gradient-to-top h-[700px]"></div>
         <Header/>
-        <div>
+        <div className='space-y-2 bg-black bg-opacity-80'>
             <section className='w-full'>
-            
-                {/* backgroung image */}
-                <div>
+                {/* background image */}
+                <div className=''>
                     <img alt='IN_BACKGROUND' src={IN_BACKGROUND_CDN}
-                        className='h-[750px] w-full object-cover'
+                        className='h-[700px] w-full object-cover'
                     />
-                    
                 </div>
-                {/* texts */}
-                <div></div>
-                {/* form */}
-                <form className=''>
-                    <div></div>
-                    <div>
-                        <input placeholder='email'/>
-                        <button>
-                            <Link>
-                                Get Staeted 
-                                <span>  </span>
-                            </Link>
-                        </button>
+
+                {/* Overlapping div */}
+                <div className='absolute top-0 left-0 w-full h-[700px] flex flex-col items-center justify-center text-white'>
+                    {/* texts */}
+                    <div className='text-center'>
+                        <h1 className='text-4xl font-black'>The biggest Indian hits. The best Indian stories. All streaming here.</h1>
+                        <p className='text-xl mt-4'>Watch anywhere. Cancel anytime.</p>
                     </div>
-                </form>
+                    {/* form */}
+                    <form className='mt-8'>
+                        <div className='flex justify-center flex-col items-center'>
+                            <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+                            <div>
+                                <input className='border p-2 mr-2' placeholder='email'/>
+                                <button className='p-2 bg-netflix-red'>
+                                    <Link className='flex items-center'>
+                                        Get Started 
+                                        <span>
+                                            <ChevronRightIcon className='w-4 h-4'/>
+                                        </span>
+                                    </Link>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </section>
+            
+            <section className='w-full h-[640px] bg-black'></section>
         </div>
     </div>
   )
